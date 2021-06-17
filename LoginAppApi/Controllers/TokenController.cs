4120a807b6b1e26e8baae6629606b14406410c46
DESKTOP-GRU0P8A\Leo <leogabri61@hotmail.com>
2021-06-17 12:01:49 -0300

    Created project and added main logic.

---
+++ LoginAppApi/Controllers/TokenController.cs
@@ -0,0 +1,30 @@
+﻿using LoginAppApi.Login;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAppApi.Controllers
+{
+    [Route("token")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return new JsonResult(new { Result = "This is a GET response." });
+        }
+        
+        [HttpPost]
+        public Token Post(LoginData loginData)
+        {
+            return new Token()
+            {
+                AccessToken = "KAlksdoopPoaspdoew"
+            };
+        }
+    }
+}
